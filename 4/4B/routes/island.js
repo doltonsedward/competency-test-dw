8a00a3ccf9c46f74ce3e89d613787f79a3507a4f
D3NCyan <edwarddoltons@gmail.com>
2021-10-14 22:36:31 +0800

    fix all task

--- 4/4B/routes/island.js
+++ 4/4B/routes/island.js
@@ -1,6 +1,8 @@
 const dbConnection = require('../connection/db')
 const uploadFile = require("../middlewares/uploadFile")
 const router = require('express').Router()
+// const fs = require('fs')
+// const path = require('path')
 
 router.get('/province/detail/:id', function(req, res) {
     const {id} = req.params
@@ -23,6 +25,33 @@ router.get('/province/detail/:id', function(req, res) {
             const detailId = req.params.id
 
             res.render('island/detail-province', {title: 'All you want to know', island, detailId})
+        })
+
+        conn.release()
+    })
+})
+
+router.get('/district/detail/:id', function(req, res) {
+    const {id} = req.params
+    const query = `
+    SELECT * FROM kabupaten_tb
+    WHERE id = ?
+    `
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [id], (err, results) => {
+            if (err) throw err
+
+            const island = []
+            for (let result of results) {
+                island.push({...result})
+            }
+
+            const detailId = req.params.id
+
+            res.render('island/detail-district', {title: 'All you want to know', island, detailId})
         })
 
         conn.release()
@@ -109,6 +138,116 @@ router.get('/province/delete/:id', function(req, res) {
         if (err) throw err
 
         conn.query(query, [id], (err, results) => {
+            if (err) throw err
+
+
+            res.redirect('/')
+        })
+
+        conn.release()
+    })
+})
+
+router.get('/province/update/:id', function(req, res) {
+    const {id} = req.params
+    const query = 'SELECT * FROM provinsi_tb WHERE id = ?'
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [id], (err, results) => {
+            if (err) throw err
+
+            const provinces = []
+            for (let result of results) {
+                provinces.push({...result})
+            }
+
+            res.render('island/update-province', {title: 'Add data of district', provinces, id})
+        })
+
+        conn.release()
+    })
+})
+
+router.post('/province/update/:id', uploadFile("image"), function(req, res) {
+    const {id} = req.params
+    const {name, inaugurated, island} = req.body
+    const image = req.file.filename
+    const query = `
+    UPDATE provinsi_tb
+    SET nama = ?, diresmikan = ?, photo = ?, pulau = ?
+    WHERE id = ?
+    `
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [name, inaugurated, image, island, id], (err, results) => {
+            if (err) throw err
+
+            res.redirect('/')
+        })
+
+        conn.release()
+    })
+})
+
+router.get('/district/delete/:id', function(req, res) {
+    const {id} = req.params
+    console.log(req.file);
+
+    const query = `DELETE FROM kabupaten_tb WHERE id = ?`
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [id], (err, results) => {
+            if (err) throw err
+
+            res.redirect('/')
+        })
+
+        conn.release()
+    })
+})
+
+router.get('/district/update/:id', function(req, res) {
+    const {id} = req.params
+    const query = 'SELECT * FROM kabupaten_tb WHERE id = ?'
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [id], (err, results) => {
+            if (err) throw err
+
+            const provinces = []
+            for (let result of results) {
+                provinces.push({...result})
+            }
+
+            res.render('island/update-district', {title: 'Add data of district', provinces, id})
+        })
+
+        conn.release()
+    })
+})
+
+router.post('/district/update/:id', uploadFile("image"), function(req, res) {
+    const {id} = req.params
+    const {name, inaugurated} = req.body
+    const image = req.file.filename
+    const query = `
+    UPDATE kabupaten_tb
+    SET nama = ?, diresmikan = ?, photo = ?
+    WHERE id = ?
+    `
+
+    dbConnection.getConnection((err, conn) => {
+        if (err) throw err
+
+        conn.query(query, [name, inaugurated, image, id], (err, results) => {
             if (err) throw err
 
             res.redirect('/')
